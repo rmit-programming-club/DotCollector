1b139b2bf015b3ba18cc61254f1b21ca8fce5feb
Tomato150 <tomcat.frantz@gmail.com>
2018-03-31 12:32:34 +1100

    Webpack dev server implementation.
    Updated package.json and webpack config accordingly to allow for usage of a webpack development server. To run, use command 'start.'

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,29 +1,64 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
     entry: {
         teacher: "./src/teacher/index.js",
         student: "./src/student/index.js"
     },
     output: {
-        path: './dist',
+        path: path.resolve(__dirname, '/dist'),
+        publicPath: "dist",
         filename: "[name]-bundle.js"
     },
     devtool: "source-map",
+    devServer: {
+        proxy: { // proxy URLs to backend development server
+            '/api': 'http://localhost:3000'
+        },
+        contentBase: './html',
+        hot: false, // hot module replacement. Depends on HotModuleReplacementPlugin
+        https: false, // true for self-signed, object for cert authority
+        noInfo: true // only errors & warns on hot reload
+    },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js?$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react', 'stage-0'],
-                    plugins: ['transform-decorators-legacy', 'transform-class-properties']
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        // presets: ['@babel/preset-env']
+                        // plugins: ['@babel/plugin-transform-runtime']
+                        presets: ['es2015', 'react', 'stage-0'],
+                        plugins: ['transform-decorators-legacy', 'transform-class-properties']
+                    }
                 },
                 exclude: /node_modules/
             }, {
                 test: /\.(png|jpg|jpeg)?$/,
-                loader: "file-loader?name=images/[name].[ext]"
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "resources/images/[name].[ext]"
+                    }
+                }
+            }, {
+                test: /\.sass$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }, {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }, {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "resources/fonts/[name].[ext]"
+                    }
+                }
             }
-        ]
+        ],
     },
     plugins: [],
     watch: false,
