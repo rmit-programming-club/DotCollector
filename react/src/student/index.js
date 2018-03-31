f2ae17b72d108c9fee222cd7ae0bfb3e0f7c635c
Tomato150 <tomcat.frantz@gmail.com>
2018-03-31 12:48:23 +1100

    SASS implementation and basic splash.

--- react/src/student/index.js
+++ react/src/student/index.js
@@ -1,11 +1,27 @@
 import React, {Component} from "react";
 import ReactDOM from 'react-dom';
 
+import 'semantic-ui-css/semantic.min.css';
+import {Header, Image} from 'semantic-ui-react';
+
+import DotLogo from '../../../resources/dotlogo.png';
+import '../css/sass/index.sass';
+
 
 class Student extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
     render() {
+        /* TODO make local styles into classes. */
         return (
-            <div><p>Student index ftw!</p></div>
+            <div className={"trial"}>
+                <Image src={DotLogo} style={{display: "block", margin: "0 auto"}}/>
+                <Header size="huge" className={"primary-text"} style={{"font-size": "4em"}}>Dot Collector</Header>
+            </div>
         )
     }
 }
