3aef269c801c23de7b562599878ee981d22e4e46
Sam Nolan <samnolan555@gmail.com>
2018-04-02 11:23:47 +1000

    Fix images not resolving

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: "dist",
+        publicPath: "../dist/",
         filename: "[name]-bundle.js"
     },
     devtool: "source-map",
