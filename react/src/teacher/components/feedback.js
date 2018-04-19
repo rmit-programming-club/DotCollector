7af5a8a9f611053377e8b8214faae35e2665aec7
Sam Nolan <samnolan555@gmail.com>
2018-04-19 15:43:44 +1000

    Add basic session posting

---
+++ react/src/teacher/components/feedback.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Header, Button} from 'semantic-ui-react';
+
+export default class SplashScreen extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+
+    componentDidMount(){
+    }
+
+    render() {
+      return (
+        <div className="feedback-screen">
+          <Header>Go to</Header>
+          <Header><a href="dot.hazelfire.org/student.html">dot.hazelfire.org/student.html</a></Header>
+          <Header>and enter the following code into your device</Header>
+          <Header>{this.props.code}</Header>
+          <Button OnClick={this.props.onFeedback}>To feedback</Button>
+        </div>
+      );
+    }
+}
