523975091e9e1f66ed2d9250912663f56b88e320
Sam Nolan <samnolan555@gmail.com>
2018-04-27 12:19:06 +1000

    Add timeout to feedback

--- react/src/student/containers/feedback.js
+++ react/src/student/containers/feedback.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Grid, Container, Image} from 'semantic-ui-react';
+import {Header, Dimmer, Loader, Grid, Container, Image} from 'semantic-ui-react';
 
 import Dotlogo from '../../../resources/dotlogo.png'
 import Question from '../components/question-box';
@@ -13,38 +13,34 @@ const QUESTIONS = [
         title: "Do you understand the content?",
         min: "Not at all.",
         max: "Wholly."
-    }, {
-        title: "Do you understand the content?2",
-        min: "Not at all.",
-        max: "Wholly."
-    }, {
-        title: "Do you understand the content?3",
-        min: "Not at all.",
-        max: "Wholly."
-    }, {
-        title: "Do you understand the content?4",
-        min: "Not at all.",
-        max: "Wholly."
-    }, {
-        title: "Do you understand the content?5",
-        min: "Not at all.",
-        max: "Wholly."
-    }
-];
+    }];
 
 const sessionsEndpoint = 'https://api.dot.hazelfire.org/sessions/'
 
 export default class Feedback extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+          sending: false,
+          timout: 0
+        };
     }
 
     render() {
         const {session} = this.props;
         const {accessCode, name, id} = session;
+        const {sending, timeout} = this.state;
         // TODO adjust styling here
+        
+        const dimmer = (
+            <Dimmer active>
+                <Loader />
+                <Header inverted>Please wait {timeout} seconds to enter feedback</Header>
+            </Dimmer>
+        );
         return (
             <div>
+                {sending && dimmer}
                 <Container textAlign="center">
                     <p className="primary-text">Your current session is {accessCode}: {name}</p>
                 </Container>
@@ -53,16 +49,38 @@ export default class Feedback extends Component {
                         {QUESTIONS.map((question, i) => <Question onFeedback={(feedback)=>this.sendFeedback(question, feedback)} question={question} key={i}/>)}
                     </Grid.Row>
                     <Grid.Row only="mobile">
-                        {QUESTIONS.map((question, i) => <Question question={question} key={i} isMobile/>)}
+                        {QUESTIONS.map((question, i) => <Question onFeedback={(feedback)=>this.sendFeedback(question, feedback)} question={question} key={i} isMobile/>)}
                     </Grid.Row>
                 </Grid>
             </div>
         )
     }
+    
+    timeoutTick = () =>{
+        let timeout = this.state.timeout;
+        timeout--;
+        if(timeout == 0){
+            this.setState({
+                sending: false,
+                timeout: 0
+            });
+            clearInterval(this.intervalId);
+        }
+        else{
+            this.setState({
+                timeout: timeout
+            });
+        }
+    }
 
     sendFeedback = (question, feedback)=>{
         const {session} = this.props;
         const {id} = session;
+        this.setState({
+            sending: true,
+            timeout: 60
+        });
+        this.intervalId = setInterval(this.timeoutTick, 1000);
 
         const endpoint = sessionsEndpoint + id + "/feedback"
 
